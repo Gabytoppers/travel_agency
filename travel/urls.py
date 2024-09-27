aceeafa5a586fc1f06b964edb8391d31ab8f5b12
gabriela pereira <gabydev.pereira@gmail.com>
2024-09-27 00:19:20 -0500

    interfaz home

--- travel/urls.py
+++ travel/urls.py
@@ -1,11 +1,9 @@
 # travel/urls.py
 from django.urls import path
 
-from django.contrib import admin
 from .views import contact, home, trip_details
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # Ruta para el administrador
     path('contact/', contact, name='contact'),
     path('', home, name='home'),
     path('trip-details/', trip_details, name='trip_details'),
